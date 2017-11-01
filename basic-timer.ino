7ac2d100e422d2e64e2cf8ede3931ffcdd85004c
Samuel Quintana <sock.sqt@gmail.com>
2017-10-31 23:33:23 -0600

    Refactor, added buttons and big 7 segments displays

--- basic-timer.ino
+++ basic-timer.ino
@@ -1,170 +1,175 @@
-#include <DS3232RTC.h>        //http://github.com/JChristensen/DS3232RTC
-#include <Streaming.h>        //http://arduiniana.org/libraries/streaming/
-#include <Time.h>             //http://playground.arduino.cc/Code/Time
-#include <Wire.h>             //http://arduino.cc/en/Reference/Wire
+#define BUTTON1_PIN               2  // Button 1, setting mode
+#define BUTTON2_PIN               3  // Button 2, decrease
+#define BUTTON3_PIN               4  // Button 3, increase
+
+#define DEFAULT_LONGPRESS_LEN    25  // Min nr of loops for a long press
+#define DELAY                    20  // Delay per loop in ms
+
+//////////////////////////////////////////////////////////////////////////////
+
+enum { EV_NONE=0, EV_SHORTPRESS, EV_LONGPRESS };
+
+//////////////////////////////////////////////////////////////////////////////
+// Class definition
+class ButtonHandler {
+
+  public:
+    // Constructor
+    ButtonHandler(int pin, int longpress_len=DEFAULT_LONGPRESS_LEN);
+
+    // Initialization done after construction, to permit static instances
+    void init();
+
+    // Handler, to be called in the loop()
+    int handle();
+
+  protected:
+    boolean was_pressed;     // previous state
+    int pressed_counter;     // press running duration
+    const int pin;           // pin to which button is connected
+    const int longpress_len; // longpress duration
+};
+
+ButtonHandler::ButtonHandler(int p, int lp)
+  : pin(p), longpress_len(lp) {
+}
+
+void ButtonHandler::init() {
+  pinMode(pin, INPUT);
+  digitalWrite(pin, HIGH); // pull-up
+  was_pressed = false;
+  pressed_counter = 0;
+}
+
+int ButtonHandler::handle() {
+  int event;
+  int now_pressed = !digitalRead(pin);
+
+  if (!now_pressed && was_pressed) {
+    // handle release event
+    if (pressed_counter < longpress_len) {
+      event = EV_SHORTPRESS;
+    } else {
+      event = EV_LONGPRESS;
+    }
+  } else {
+    event = EV_NONE;
+  }
+
+  // update press running duration
+  if (now_pressed) {
+    ++pressed_counter;
+  } else {
+    pressed_counter = 0;
+  }
+
+  // remember state, and we're done
+  was_pressed = now_pressed;
+  return event;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+#include <DS3231.h>
 #include <TM1637Display.h>
 
-#define CLK 4
-#define DIO 5
-#define PIN_RELE 2
+#define CLK 9 //can be any digital pin
+#define DIO 8 //can be any digital pin
 
-#define INIT_HOUR 19
-#define END_HOUR 7
-
+// Init the DS3231 using the hardware interface
+DS3231  rtc(SDA, SCL);
 TM1637Display display(CLK, DIO);
-uint8_t data[] = { 0x00, 0x38, 0x79, 0x3f };
+Time curr_time;
 
-void setup(void) {
-  Serial.begin(115200);
+// Instantiate button objects
+ButtonHandler btn_setup(BUTTON1_PIN, DEFAULT_LONGPRESS_LEN * 2);
+ButtonHandler btn_decrease(BUTTON2_PIN);
+ButtonHandler btn_increase(BUTTON3_PIN);
+
+boolean setup_mode = false;
+boolean setup_hours = false;
+boolean setup_minutes = false;
+
+byte bcd_map[] = {
+  0b0000, // 0
+  0b0001, // 1
+  0b0010, // 2
+  0b0011, // 3
+  0b0100, // 4
+  0b0101, // 5
+  0b0110, // 6
+  0b0111, // 7
+  0b1000, // 8
+  0b1001, // 9
+  };
+
+void setup() {
+  Serial.begin(9600);
+
+  rtc.begin();
+  display.setBrightness(0x0a);
+
+  // init buttons pins; I suppose it's best to do here
+  btn_setup.init();
+  btn_decrease.init();
+  btn_increase.init();
+}
+
+void print_current_time() {
+  curr_time = rtc.getTime();
+
+  uint8_t data[] = {0x0, 0x0, 0x0, 0x0};
   
-  display.setBrightness(0x0f);
-  display.setSegments(data);
-  delay(5000);
-  
-  //setSyncProvider() causes the Time library to synchronize with the
-  //external RTC by calling RTC.get() every five minutes by default.
-  setSyncProvider(RTC.get);
-  Serial << F("RTC Sync");
-
-  if (timeStatus() != timeSet) {
-    Serial << F(" FAIL!");
-  } else {
-    // minutes, hours, dayOrDate);
-    RTC.setAlarm(ALM2_MATCH_HOURS, 0, 19, 0);
-    RTC.alarm(ALARM_2);
-    RTC.alarmInterrupt(ALARM_2, true);
-
-    // seconds, minutes, hours, dayOrDate);
-    RTC.setAlarm(ALM1_MATCH_HOURS, 0, 0, 7, 0);
-    RTC.alarm(ALARM_1);
-    RTC.alarmInterrupt(ALARM_1, true);
-
-    int currentHour = hour(now());
-    Serial << "current hour: " << currentHour << endl;
-
-    pinMode(PIN_RELE, OUTPUT);
-    if (currentHour > END_HOUR && currentHour < INIT_HOUR) {
-      Serial << F("Setting OFF the rele") << endl;
-      digitalWrite(PIN_RELE, HIGH);
-    }
-  }
-
-  Serial << endl;
-}
-
-void loop(void) {
-  static time_t tLast;
-  time_t t;
-  tmElements_t tm;
-
-  //check for input to set the RTC, minimum length is 12, i.e. yy,m,d,h,m,s
-  if (Serial.available() >= 12) {
-    //note that the tmElements_t Year member is an offset from 1970,
-    //but the RTC wants the last two digits of the calendar year.
-    //use the convenience macros from Time.h to do the conversions.
-    int y = Serial.parseInt();
-    if (y >= 100 && y < 1000) {
-      Serial << F("Error: Year must be two digits or four digits!") << endl;
-    } else {
-      if (y >= 1000) {
-        tm.Year = CalendarYrToTm(y);
-      } else { //(y < 100)
-        tm.Year = y2kYearToTm(y);
-      }
-      tm.Month = Serial.parseInt();
-      tm.Day = Serial.parseInt();
-      tm.Hour = Serial.parseInt();
-      tm.Minute = Serial.parseInt();
-      tm.Second = Serial.parseInt();
-      t = makeTime(tm);
-      RTC.set(t);        //use the time_t value to ensure correct weekday is set
-      setTime(t);        
-      Serial << F("RTC set to: ");
-      printDateTime(t);
-      Serial << endl;
-
-      //dump any extraneous input
-      while (Serial.available() > 0) Serial.read();
+  if (setup_mode) {
+    if (setup_hours) {
+      data[0] = 0;
+      data[1] = 0;
+    } else if (setup_minutes) {
+      data[2] = 0;
+      data[3] = 0;
     }
   } else {
-    if (RTC.alarm(ALARM_2)) {
-      Serial.println("Matches 19 Hours");
-      // Turn on
-      digitalWrite(PIN_RELE, LOW);
+    
+  }
+}
+
+void loop() {
+  // handle button
+  int event_set = btn_setup.handle();
+  int event_dec = btn_decrease.handle();
+  int event_inc = btn_increase.handle();
+
+  if (event_set == EV_LONGPRESS && !setup_mode) {
+    setup_mode = true;
+    setup_hours = true;
+  } else if (event_set == EV_SHORTPRESS && setup_mode) {
+    setup_hours = !setup_hours;
+    setup_minutes = !setup_minutes;
+  } else if (setup_mode && event_set == EV_LONGPRESS) {
+    // stop blinking
+    setup_mode = false;
+    setup_hours = false;
+    setup_minutes = false;
+  } else if (event_dec == EV_SHORTPRESS && setup_mode) {
+    curr_time = rtc.getTime();
+    
+    if (setup_hours) {
+      rtc.setTime(curr_time.hour - 1, curr_time.min, curr_time.sec);
+    } else if (setup_minutes) {
+      rtc.setTime(curr_time.hour, curr_time.min - 1, curr_time.sec);
     }
-    if (RTC.alarm(ALARM_1)) {
-      Serial.println("Matches 07 Hours");
-      // Turn off
-      digitalWrite(PIN_RELE, HIGH);
+  } else if (event_inc == EV_SHORTPRESS && setup_mode) {
+    curr_time = rtc.getTime();
+    
+    if (setup_hours) {
+      rtc.setTime(curr_time.hour + 1, curr_time.min, curr_time.sec);
+    } else if (setup_minutes) {
+      rtc.setTime(curr_time.hour, curr_time.min + 1, curr_time.sec);
     }
   }
-  
-  t = now();
-  if (t != tLast) {
-    tLast = t;
-    printDateTime(t);
-    if (second(t) == 0) {
-      float c = RTC.temperature() / 4.;
-      float f = c * 9. / 5. + 32.;
-      Serial << F("  ") << c << F(" C  ") << f << F(" F");
-    }
-    Serial << endl;
-  }
-}
 
-//print date and time to Serial
-void printDateTime(time_t t) {
-  printDate(t);
-  Serial << ' ';
-  printTime(t);
-}
+  print_current_time();
 
-//print time to Serial
-void printTime(time_t t) {
-  printI00(hour(t), ':');
-  printI00(minute(t), ':');
-  printI00(second(t), ' ');
-
-  printToDisplay(hour(t), minute(t));
-}
-
-void printToDisplay(int hours, int minutes) {
-  if (hours < 10) {
-    data[0] = 0x00;
-    data[1] = display.encodeDigit(hours);
-  } else {
-    data[0] = display.encodeDigit(hours / 10);
-    data[1] = display.encodeDigit(hours % 10);
-  }
-
-  if (minutes < 10) {
-    data[2] = 0x3f;
-    data[3] = display.encodeDigit(minutes);
-  } else {
-    data[2] = display.encodeDigit(minutes / 10);
-    data[3] = display.encodeDigit(minutes % 10);
-  }
-
-  display.setSegments(data);
-}
-
-//print date to Serial
-void printDate(time_t t) {
-  printI00(day(t), 0);
-  Serial << monthShortStr(month(t)) << _DEC(year(t));
-}
-
-//Print an integer in "00" format (with leading zero),
-//followed by a delimiter character to Serial.
-//Input value assumed to be between 0 and 99.
-void printI00(int val, char delim) {
-  if (val < 10) {
-    Serial << '0';
-  }
-  Serial << _DEC(val);
-  if (delim > 0) {
-    Serial << delim;
-  }
-  return;
+  delay(DELAY);
 }
 
