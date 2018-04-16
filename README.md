2f1215bc363d7803dcb696aa64d103dd575371ff
Samuel Quintana <samuel.quintana@globant.com>
2018-04-16 16:03:21 -0500

    Integrated ds3231 and tm1637 modules

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # basic-timer
 Arduino basic timer with RELE
+
+# Dependencies
+ * https://github.com/avishorp/TM1637.git
+ * https://github.com/NorthernWidget/DS3231
+
--- basic-timer.ino
+++ basic-timer.ino
@@ -1,21 +1,21 @@
-#define BUTTON1_PIN               2  // Button 1, setting mode
-#define BUTTON2_PIN               3  // Button 2, decrease
-#define BUTTON3_PIN               4  // Button 3, increase
+#define BTN_HOURS_PIN            2  // Button 1
+#define BTN_MINUTES_PIN          3  // Button 2
 
 #define DEFAULT_LONGPRESS_LEN    25  // Min nr of loops for a long press
 #define DELAY                    20  // Delay per loop in ms
 
 //////////////////////////////////////////////////////////////////////////////
 
-enum { EV_NONE=0, EV_SHORTPRESS, EV_LONGPRESS };
+enum { EV_NONE = 0, EV_SHORTPRESS, EV_LONGPRESS };
 
 //////////////////////////////////////////////////////////////////////////////
 // Class definition
+
 class ButtonHandler {
 
   public:
     // Constructor
-    ButtonHandler(int pin, int longpress_len=DEFAULT_LONGPRESS_LEN);
+    ButtonHandler(int pin, int longpress_len = DEFAULT_LONGPRESS_LEN);
 
     // Initialization done after construction, to permit static instances
     void init();
@@ -30,9 +30,7 @@ class ButtonHandler {
     const int longpress_len; // longpress duration
 };
 
-ButtonHandler::ButtonHandler(int p, int lp)
-  : pin(p), longpress_len(lp) {
-}
+ButtonHandler::ButtonHandler(int p, int lp) : pin(p), longpress_len(lp) { }
 
 void ButtonHandler::init() {
   pinMode(pin, INPUT);
@@ -47,21 +45,13 @@ int ButtonHandler::handle() {
 
   if (!now_pressed && was_pressed) {
     // handle release event
-    if (pressed_counter < longpress_len) {
-      event = EV_SHORTPRESS;
-    } else {
-      event = EV_LONGPRESS;
-    }
+    event = pressed_counter < longpress_len ? EV_SHORTPRESS : EV_LONGPRESS;
   } else {
     event = EV_NONE;
   }
 
   // update press running duration
-  if (now_pressed) {
-    ++pressed_counter;
-  } else {
-    pressed_counter = 0;
-  }
+  pressed_counter = now_pressed ? pressed_counter + 1 : 0;
 
   // remember state, and we're done
   was_pressed = now_pressed;
@@ -77,98 +67,74 @@ int ButtonHandler::handle() {
 #define DIO 8 //can be any digital pin
 
 // Init the DS3231 using the hardware interface
-DS3231  rtc(SDA, SCL);
 TM1637Display display(CLK, DIO);
-Time curr_time;
+DS3231 rtc;
 
 // Instantiate button objects
-ButtonHandler btn_setup(BUTTON1_PIN, DEFAULT_LONGPRESS_LEN * 2);
-ButtonHandler btn_decrease(BUTTON2_PIN);
-ButtonHandler btn_increase(BUTTON3_PIN);
-
-boolean setup_mode = false;
-boolean setup_hours = false;
-boolean setup_minutes = false;
-
-byte bcd_map[] = {
-  0b0000, // 0
-  0b0001, // 1
-  0b0010, // 2
-  0b0011, // 3
-  0b0100, // 4
-  0b0101, // 5
-  0b0110, // 6
-  0b0111, // 7
-  0b1000, // 8
-  0b1001, // 9
-  };
+ButtonHandler btn_hours(BTN_HOURS_PIN);
+ButtonHandler btn_minutes(BTN_MINUTES_PIN);
+DateTime curr_time;
+RTClib RTC;
 
 void setup() {
   Serial.begin(9600);
 
-  rtc.begin();
+  // Initialize the rtc object
+  Wire.begin();
   display.setBrightness(0x0a);
 
-  // init buttons pins; I suppose it's best to do here
-  btn_setup.init();
-  btn_decrease.init();
-  btn_increase.init();
+  btn_hours.init();
+  btn_minutes.init();
 }
 
 void print_current_time() {
-  curr_time = rtc.getTime();
+  curr_time = RTC.now();
+  int current_hour = curr_time.hour();
+  int current_minute = curr_time.minute();
 
-  uint8_t data[] = {0x0, 0x0, 0x0, 0x0};
-  
-  if (setup_mode) {
-    if (setup_hours) {
-      data[0] = 0;
-      data[1] = 0;
-    } else if (setup_minutes) {
-      data[2] = 0;
-      data[3] = 0;
-    }
-  } else {
-    
-  }
+  uint8_t data[] = {
+    current_hour / 10,
+    current_hour % 10,
+    current_minute / 10,
+    current_minute % 10
+  };
+
+  display.setSegments(data);
+}
+
+void print_temperature() {
+  int temperature = (int) rtc.getTemperature();
+  uint8_t data[] = {
+    temperature / 10,
+    temperature % 10,
+    0x63,
+    0x0
+  };
+  display.setSegments(data);
 }
 
 void loop() {
+  rtc.setClockMode(false);  // set to 24h
+  
   // handle button
-  int event_set = btn_setup.handle();
-  int event_dec = btn_decrease.handle();
-  int event_inc = btn_increase.handle();
+  int event_btn_hours = btn_hours.handle();
+  int event_btn_minutes= btn_minutes.handle();
 
-  if (event_set == EV_LONGPRESS && !setup_mode) {
-    setup_mode = true;
-    setup_hours = true;
-  } else if (event_set == EV_SHORTPRESS && setup_mode) {
-    setup_hours = !setup_hours;
-    setup_minutes = !setup_minutes;
-  } else if (setup_mode && event_set == EV_LONGPRESS) {
-    // stop blinking
-    setup_mode = false;
-    setup_hours = false;
-    setup_minutes = false;
-  } else if (event_dec == EV_SHORTPRESS && setup_mode) {
-    curr_time = rtc.getTime();
-    
-    if (setup_hours) {
-      rtc.setTime(curr_time.hour - 1, curr_time.min, curr_time.sec);
-    } else if (setup_minutes) {
-      rtc.setTime(curr_time.hour, curr_time.min - 1, curr_time.sec);
-    }
-  } else if (event_inc == EV_SHORTPRESS && setup_mode) {
-    curr_time = rtc.getTime();
-    
-    if (setup_hours) {
-      rtc.setTime(curr_time.hour + 1, curr_time.min, curr_time.sec);
-    } else if (setup_minutes) {
-      rtc.setTime(curr_time.hour, curr_time.min + 1, curr_time.sec);
-    }
+  if (event_btn_hours == EV_LONGPRESS) {
+    int current_hour = curr_time.hour();
+    int new_hour = current_hour < 24 ? current_hour + 1 : 0;
+    rtc.setHour(new_hour);
+  } else if (event_btn_minutes == EV_LONGPRESS) {
+    int current_minute = curr_time.minute();
+    int new_minute = current_minute < 60 ? current_minute + 1 : 0;
+    rtc.setMinute(new_minute);
   }
 
   print_current_time();
+
+  if (curr_time.second() >= 57) {
+    print_temperature();
+  }
 
   delay(DELAY);
 }
